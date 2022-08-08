7058875b36679744f0cc5eff99556f69043ce6c2
Andrew Kuskov <qvant86@gmail.com>
2022-08-09 01:28:37 +0300

    first test version
    It works,, but I need do a bit more research on spring boot test

---
+++ homework_4/src/test/java/ru/otus/spring/exam/test/TestExamApplication.java
@@ -0,0 +1,16 @@
+package ru.otus.spring.exam.test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest//(webEnvironment= SpringBootTest.WebEnvironment.MOCK)
+class TestExamApplication {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
