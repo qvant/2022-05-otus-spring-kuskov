27ff8330201c2e7821cbb15a0e0baa4fcd54159a
Andrew Kuskov <qvant86@gmail.com>
2022-08-23 01:16:15 +0300

    removed useless mock

--- homework_3/src/test/java/ru/otus/spring/exam/TestExamApplication.java
+++ homework_3/src/test/java/ru/otus/spring/exam/TestExamApplication.java
@@ -1,8 +1,6 @@
 package ru.otus.spring.exam;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import ru.otus.spring.exam.service.IOService;
@@ -13,10 +11,6 @@ class TestExamApplication {
     @MockBean
     private IOService ioService;
 
-    @BeforeEach
-    void SetUp(){
-        Mockito.when(ioService.read()).thenReturn("A");
-    }
     @Test
     void contextLoads() {
 
