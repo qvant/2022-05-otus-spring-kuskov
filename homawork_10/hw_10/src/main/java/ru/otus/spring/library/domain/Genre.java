6c9d5b8434d9579380d5329726b27292e1bc7e61
Andrew Kuskov <qvant86@gmail.com>
2022-10-23 17:20:44 +0300

    9th homework as is

---
+++ homawork_10/hw_10/src/main/java/ru/otus/spring/library/domain/Genre.java
@@ -0,0 +1,42 @@
+package ru.otus.spring.library.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "genres")
+@SequenceGenerator(name = "S_GENRES", sequenceName = "S_GENRES", initialValue = 999, allocationSize = 1)
+public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "S_GENRES")
+    private long id;
+    private String name;
+
+    public Genre() {
+
+    }
+
+    public Genre(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Genre(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
