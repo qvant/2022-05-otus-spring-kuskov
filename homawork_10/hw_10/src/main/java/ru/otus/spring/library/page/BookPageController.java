779a93fc9bce6359a3b6e7ca9a2ea3356336a627
Andrew Kuskov <qvant86@gmail.com>
2022-11-03 02:12:50 +0300

    removed unused code

--- homawork_10/hw_10/src/main/java/ru/otus/spring/library/page/BookPageController.java
+++ homawork_10/hw_10/src/main/java/ru/otus/spring/library/page/BookPageController.java
@@ -5,16 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import ru.otus.spring.library.service.AuthorService;
-import ru.otus.spring.library.service.BookService;
-import ru.otus.spring.library.service.GenreService;
 
 @RequiredArgsConstructor
 @Controller
 public class BookPageController {
-    private final BookService bookService;
-    private final AuthorService authorService;
-    private final GenreService genreService;
 
     @GetMapping("/books")
     public String listPage(Model model) {
--- homawork_10/hw_10/src/main/java/ru/otus/spring/library/page/CommentPageController.java
+++ homawork_10/hw_10/src/main/java/ru/otus/spring/library/page/CommentPageController.java
@@ -3,13 +3,11 @@ package ru.otus.spring.library.page;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.otus.spring.library.service.CommentService;
 
 @Controller
 @RequiredArgsConstructor
 public class CommentPageController {
 
-    private final CommentService commentService;
 
     @GetMapping("/comments")
     public String listPage() {
