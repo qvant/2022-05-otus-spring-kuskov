1ad94c7ab7de9c08a658018b8037a199dade5e5c
Andrew Kuskov <qvant86@gmail.com>
2022-11-09 23:13:57 +0300

    task instances ui

---
+++ project/ProcessOrchestratorCore/src/main/java/ru/otus/spring/orchestrator/core/page/TaskInstancesPageController.java
@@ -0,0 +1,18 @@
+package ru.otus.spring.orchestrator.core.page;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class TaskInstancesPageController {
+
+    @GetMapping("/taskInstances")
+    public String listPage(Model model) {
+        return "taskInstancesList";
+    }
+
+}
