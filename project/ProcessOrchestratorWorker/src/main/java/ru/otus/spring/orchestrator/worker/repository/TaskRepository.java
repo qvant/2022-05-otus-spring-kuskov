5163b228bba658b56fe90918b5ae3d2db1d951b3
Andrew Kuskov <qvant86@gmail.com>
2022-11-20 20:20:41 +0300

    Delete TaskRepository.java

--- project/ProcessOrchestratorWorker/src/main/java/ru/otus/spring/orchestrator/worker/repository/TaskRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.spring.orchestrator.worker.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.otus.spring.orchestrator.worker.domain.Task;
-
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
