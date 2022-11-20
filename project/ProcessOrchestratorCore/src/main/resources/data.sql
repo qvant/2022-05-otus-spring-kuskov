e76dc41d096a2793953700ebcde22d878db7a94e
Andrew Kuskov <qvant86@gmail.com>
2022-11-20 20:20:45 +0300

    Update data.sql

--- project/ProcessOrchestratorCore/src/main/resources/data.sql
+++ project/ProcessOrchestratorCore/src/main/resources/data.sql
@@ -15,4 +15,6 @@ insert into tasks (id, name, code, next_run, schedule_id, task_type_id) values (
 insert into dependencies(id , dependency_type_id, task_parent_id , task_id) values (-1, 1, -1, -6);
 insert into tasks (id, name, code, next_run, schedule_id, task_type_id) values (-7, 'Test task, running on failure', 'select null from dual', null,  null, 1);
 insert into dependencies(id , dependency_type_id, task_parent_id , task_id) values (-2, 2, -4, -7);
+
+insert into tasks (id, name, code, next_run, schedule_id, task_type_id) values (-8, 'Test Postgres task ', 'update process_test.eods set dt_eod = dt_eod + 1', '2022-10-01',  -1, 3);
 commit;
